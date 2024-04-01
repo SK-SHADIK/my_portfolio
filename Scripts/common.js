c36c99386922bc6ac7dc7a38231060dc4e71ae58
sk shadik <57946665+SK-SHADIK@users.noreply.github.com>
2024-04-01 23:34:47 +0600

    Add Comment

--- Scripts/common.js
+++ Scripts/common.js
@@ -1,11 +1,17 @@
+// Selecting the header element
 let header = document.querySelector('.header');
 
+// Function to toggle header background color on scroll
 function toggleHeaderBackground() {
+    // Check if window is scrolled
     if (window.scrollY > 0) {
+        // Add 'header-scroll' class to change header background color
         header.classList.add('header-scroll');
     } else {
+        // Remove 'header-scroll' class to revert header background color
         header.classList.remove('header-scroll');
     }
 }
 
+// Event listener for scroll event to trigger toggleHeaderBackground function
 window.addEventListener('scroll', toggleHeaderBackground);
